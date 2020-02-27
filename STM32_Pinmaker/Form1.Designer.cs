50f02f8f3355c14ff330f7697726e27298f522b8
Dieter-Graef <dieter_graef@t-online.de>
2020-02-27 09:35:36 +0100

    Add files via upload

---
+++ STM32_Pinmaker/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Drawing.Imaging;
+namespace STM32_Pinmaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Datei = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.GPIODatei = new System.Windows.Forms.TextBox();
+            this.gpioladen = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.csvPfad = new System.Windows.Forms.Button();
+            this.csv = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cubepfad = new System.Windows.Forms.TextBox();
+            this.pfadcube = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chip = new System.Windows.Forms.TextBox();
+            this.chipload = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Datei
+            // 
+            this.Datei.Location = new System.Drawing.Point(11, 218);
+            this.Datei.Name = "Datei";
+            this.Datei.Size = new System.Drawing.Size(670, 20);
+            this.Datei.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "csv";
+            // 
+            // GPIODatei
+            // 
+            this.GPIODatei.Location = new System.Drawing.Point(12, 172);
+            this.GPIODatei.Name = "GPIODatei";
+            this.GPIODatei.Size = new System.Drawing.Size(669, 20);
+            this.GPIODatei.TabIndex = 3;
+            // 
+            // gpioladen
+            // 
+            this.gpioladen.Location = new System.Drawing.Point(700, 172);
+            this.gpioladen.Name = "gpioladen";
+            this.gpioladen.Size = new System.Drawing.Size(88, 20);
+            this.gpioladen.TabIndex = 4;
+            this.gpioladen.Text = "load";
+            this.gpioladen.UseVisualStyleBackColor = true;
+            this.gpioladen.Click += new System.EventHandler(this.gpioladen_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(274, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Chip Family - GPIO-STM32.....xml";
+            // 
+            // csvPfad
+            // 
+            this.csvPfad.Location = new System.Drawing.Point(700, 73);
+            this.csvPfad.Name = "csvPfad";
+            this.csvPfad.Size = new System.Drawing.Size(88, 22);
+            this.csvPfad.TabIndex = 6;
+            this.csvPfad.Text = "set";
+            this.csvPfad.UseVisualStyleBackColor = true;
+            this.csvPfad.Click += new System.EventHandler(this.csvPfad_Click);
+            // 
+            // csv
+            // 
+            this.csv.Location = new System.Drawing.Point(12, 75);
+            this.csv.Name = "csv";
+            this.csv.Size = new System.Drawing.Size(669, 20);
+            this.csv.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(200, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Path to save the csv file";
+            // 
+            // cubepfad
+            // 
+            this.cubepfad.Location = new System.Drawing.Point(12, 25);
+            this.cubepfad.Name = "cubepfad";
+            this.cubepfad.Size = new System.Drawing.Size(669, 20);
+            this.cubepfad.TabIndex = 9;
+            // 
+            // pfadcube
+            // 
+            this.pfadcube.Location = new System.Drawing.Point(700, 25);
+            this.pfadcube.Name = "pfadcube";
+            this.pfadcube.Size = new System.Drawing.Size(88, 22);
+            this.pfadcube.TabIndex = 10;
+            this.pfadcube.Text = "set ";
+            this.pfadcube.UseVisualStyleBackColor = true;
+            this.pfadcube.Click += new System.EventHandler(this.pfadcube_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 2);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(170, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Path to STM32Cube";
+            // 
+            // chip
+            // 
+            this.chip.Location = new System.Drawing.Point(12, 121);
+            this.chip.Name = "chip";
+            this.chip.Size = new System.Drawing.Size(669, 20);
+            this.chip.TabIndex = 12;
+            // 
+            // chipload
+            // 
+            this.chipload.Location = new System.Drawing.Point(700, 121);
+            this.chipload.Name = "chipload";
+            this.chipload.Size = new System.Drawing.Size(88, 20);
+            this.chipload.TabIndex = 13;
+            this.chipload.Text = "load";
+            this.chipload.UseVisualStyleBackColor = true;
+            this.chipload.Click += new System.EventHandler(this.chipload_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(13, 98);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(168, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Chip - STM32.....xml";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 260);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.chipload);
+            this.Controls.Add(this.chip);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pfadcube);
+            this.Controls.Add(this.cubepfad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.csv);
+            this.Controls.Add(this.csvPfad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.gpioladen);
+            this.Controls.Add(this.GPIODatei);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Datei);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox Datei;
+        private OpenFileDialog openFileDialog1;
+        private Label label1;
+        private TextBox GPIODatei;
+        private Button gpioladen;
+        private Label label2;
+        private Button csvPfad;
+        private TextBox csv;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private Label label3;
+        private TextBox cubepfad;
+        private Button pfadcube;
+        private Label label4;
+        private TextBox chip;
+        private Button chipload;
+        private Label label5;
+    }
+}
+
---
+++ STM32_Pinmaker/Form1.cs
@@ -0,0 +1,411 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace STM32_Pinmaker
+{
+    public partial class Form1 : Form
+    {
+        string Zieldatei;
+        string GPIOSuchPfad;
+        string GPIOPfad;
+        string chipSuchPfad;
+        string chipPfad;
+        string Dateiname;
+        byte[] fileBytes;
+        int filezeiger = 0;
+        char[] magicString;
+        string[] funkliste;
+        string[]altliste;
+        string[] csvline = new string[20];
+        public struct adcport
+        {
+            public string Port;
+            public string ADCs;
+        }
+        adcport[] adcports;
+        public Form1()
+        {
+            InitializeComponent();
+            gpioladen.Enabled = false;
+            csvPfad.Enabled = false;
+            chipload.Enabled = false;
+         }
+
+        private string searchadc(string srch)
+        {
+            for (int i = 0;i< adcports.Length;i++)
+            {
+                if (adcports[i].Port == srch)
+                    return adcports[i].ADCs;
+            }
+            return "";
+        }
+
+        private static bool IsMatch(byte[] array, int position, byte[] candidate)
+        {
+            if (candidate.Length > (array.Length - position))
+                return false;
+
+            for (int i = 0; i < candidate.Length; i++)
+                if (array[position + i] != candidate[i])
+                    return false;
+
+            return true;
+        }
+
+        private static int[] Locate( byte[] self, byte[] candidate)
+        {
+            var list = new List<int>();
+            int[] Empty = new int[0];
+            for (int i = 0; i < self.Length; i++)
+            {
+                if (!IsMatch(self, i, candidate))
+                    continue;
+
+                list.Add(i);
+            }
+
+            return list.Count == 0 ? Empty : list.ToArray();
+        }
+
+        private static byte[] StringToByteArray(string str)
+        {
+            System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
+            return enc.GetBytes(str);
+        }
+
+        private void gpioladen_Click(object sender, EventArgs e)
+        {
+            string such = "    <About>";
+            string ersetz1 = "<?xml version=\"1.0\" encoding =\"UTF - 8\" ?>;  <IP>";
+            string ersetz2 = ersetz1.Replace(";", Environment.NewLine);
+            string csvl1 = "Port,,AF0,AF1,AF2,AF3,AF4,AF5,AF6,AF7,AF8,AF9,AF10,AF11,AF12,AF13,AF14,AF15,";
+            string csvl2 = ",,AF0,AF1,AF2,AF3,AF4,AF5,AF6,AF7,AF8,AF9,AF10,AF11,AF12,AF13,AF14,AF15,ADC";
+            byte[] suchb = StringToByteArray(such);
+            byte[] ersetzb = StringToByteArray(ersetz2);
+            int[] position;
+            string[] line;
+            line = new string[20];
+            DialogResult result;
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.InitialDirectory = cubepfad.Text+"\\STM32CubeMX\\db\\mcu\\IP\\";
+            openFile.Filter = "CubeMCU GPIO files (GPIO-STM32*_gpio_v1_0_Modes.xml)|GPIO-STM32*_gpio_v1_0_Modes.xml";
+            openFile.FilterIndex = 2;
+            openFile.RestoreDirectory = true;
+            result = openFile.ShowDialog();
+            if (result == DialogResult.OK)
+                GPIODatei.Text = openFile.FileName;
+            if (result == DialogResult.Cancel)
+            { GPIODatei.Text = ""; return; }
+            if (GPIODatei.Text != "")
+            {
+                if (File.Exists(GPIODatei.Text))
+                {
+                    GPIOPfad = System.IO.Path.GetDirectoryName(GPIODatei.Text);
+                    GPIOSuchPfad = System.IO.Path.GetFullPath(GPIOPfad);
+                    Dateiname = System.IO.Path.GetFileNameWithoutExtension(GPIODatei.Text);
+                    Dateiname = Dateiname + ".csv";
+                    Datei.Text = Dateiname;
+                    Zieldatei = csv.Text + "\\" + Dateiname;
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(@Zieldatei, false))
+                    {
+                        file.WriteLine(csvl1);
+                        file.WriteLine(csvl2);
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Datei existiert nicht");
+                    return;
+                }
+                fileBytes = File.ReadAllBytes(GPIODatei.Text);
+                filezeiger = 0;
+                magicString = new char[5];
+                int i;
+                for (i = 0; i < 5; i++)
+                {
+                    magicString[i] = (char)fileBytes[i];
+                }
+                filezeiger = filezeiger + i;
+                string tmp1 = new string(magicString);
+                string tmp2 = "<?xml";
+                if (!(tmp1 == tmp2))
+                {
+                    MessageBox.Show("Kein xml!");
+                    return;
+                }
+                else
+                {
+                    // patch original to work
+                    position = Locate(fileBytes, suchb);
+                    int p = position[0];
+                    for(i=0;i<ersetzb.Length;i++)
+                    {
+                        fileBytes[i] = ersetzb[i];
+                    }
+                    for(i=ersetzb.Length; i<(p-1); i++)
+                    {
+                        fileBytes[i] = (byte)' ';
+                    }
+                    // patch done
+                    string XMLfile = System.Text.Encoding.UTF8.GetString(fileBytes);
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(new StringReader(XMLfile));
+                    i = 0;
+                    XmlNode r1 = doc.SelectSingleNode("/IP");
+                    XmlNodeList listOfItems = doc.SelectNodes("/IP/GPIO_Pin");
+                    foreach (XmlNode singleItem in listOfItems)
+                    {
+                        string port = singleItem.Attributes["PortName"].InnerText;
+                        string pin  = singleItem.Attributes["Name"].InnerText;
+                        var Funklist = new List<string>();
+                        var Altlist = new List<string>();
+                        foreach (XmlNode child1 in singleItem.ChildNodes)
+                        {
+                          if (child1.Name == "PinSignal")
+                            {
+                                i = 0;
+                                Funklist.Add(child1.Attributes["Name"].InnerText);
+                                foreach (XmlNode child2 in child1.ChildNodes)
+                                {
+                                    
+                                    if (child2.Name == "SpecificParameter")
+                                    {
+                                        foreach (XmlNode child3 in child2.ChildNodes)
+                                        {
+                                            if (child3.Name == "PossibleValue")
+                                            {
+                                                Altlist.Add(child3.InnerText);
+                                                i = 1;
+                                            }
+                                        }
+                                    }
+                                }
+                                if (i==0)
+                                {
+                                    Altlist.Add("");
+                                }
+                            }
+                        }
+                        funkliste = Funklist.ToArray();
+                        altliste = Altlist.ToArray();
+                        for(i=0;i<20;i++)
+                        {
+                            csvline[i] = "";
+                        }
+                        csvline[0] = "Port"+port.Remove(0,1);
+                        string srch = pin;
+                        if (srch.IndexOf("_") > 0)
+                        {
+                            srch = srch.Remove(srch.IndexOf("_"));
+                        }
+                        if (srch.IndexOf("-") > 0)
+                        {
+                            srch = srch.Remove(srch.IndexOf("-"));
+                        }
+                        string srchresult = searchadc(srch);
+                        csvline[1] = pin;
+                        csvline[17] = "EVENTOUT";
+                        csvline[18] = srchresult;
+                        for (i=0;i<funkliste.Count();i++)
+                        {
+                            string[] spl = altliste[i].Split('_');
+                            int af = -1;
+                            if (spl.Count()>0)
+                            {
+                                af = int.Parse(spl[1].Remove(0,2));
+                                af = af + 2;
+                                if (csvline[af]=="")
+                                {
+                                    csvline[af] = funkliste[i];
+                                }
+                                else
+                                {
+                                    csvline[af] = csvline[af] + "/" + funkliste[i];
+                                }
+                            }
+
+
+                        }
+                        //csvline speichern
+                        string sline;
+                        sline = "";
+                        for (i = 0; i < 19; i++)
+                        {
+                            sline = sline + csvline[i] + ",";
+                        }
+                        using (System.IO.StreamWriter file = new System.IO.StreamWriter(@Zieldatei, true))
+                        {
+                            file.WriteLine(sline);
+                        }
+                    }
+
+                }
+            }
+            csvPfad.Enabled = false;
+            gpioladen.Enabled = false;
+            chipload.Enabled = false;
+            MessageBox.Show("Done");
+        }
+
+        private void csvPfad_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog objDialog = new FolderBrowserDialog();
+            objDialog.Description = "csv Pfad";
+            objDialog.SelectedPath = @"C:\";       // Vorgabe Pfad (und danach der gewählte Pfad)
+            DialogResult objResult = objDialog.ShowDialog(this);
+            if (objResult == DialogResult.OK)
+            {
+                csv.Text = objDialog.SelectedPath;
+                chipload.Enabled = true;
+            }
+            else
+                MessageBox.Show("Abbruch gewählt!");
+
+        }
+
+        private void pfadcube_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog objDialog = new FolderBrowserDialog();
+            objDialog.Description = "STM32Cube Pfad";
+            objDialog.SelectedPath = @"C:\";       // Vorgabe Pfad (und danach der gewählte Pfad)
+            DialogResult objResult = objDialog.ShowDialog(this);
+            if (objResult == DialogResult.OK)
+            {
+                cubepfad.Text = objDialog.SelectedPath;
+                csvPfad.Enabled = true;
+            }
+            else
+                MessageBox.Show("Abbruch gewählt!");
+        }
+
+        private void chipload_Click(object sender, EventArgs e)
+        {
+            string such = "<Core>";
+            string ersetz1 = "<?xml version=\"1.0\" encoding =\"UTF - 8\" ?>;  <Mcu>";
+            string ersetz2 = ersetz1.Replace(";", Environment.NewLine);
+            byte[] suchb = StringToByteArray(such);
+            byte[] ersetzb = StringToByteArray(ersetz2);
+            int[] position;
+            string[] adcliste;
+            DialogResult result;
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.InitialDirectory = cubepfad.Text + "\\STM32CubeMX\\db\\mcu\\";
+            openFile.Filter = "CubeMCU CPU files (STM32*.xml)|STM32*.xml";
+            openFile.FilterIndex = 2;
+            openFile.RestoreDirectory = true;
+            result = openFile.ShowDialog();
+            if (result == DialogResult.OK)
+                chip.Text = openFile.FileName;
+            if (result == DialogResult.Cancel)
+            { chip.Text = ""; return; }
+            if (chip.Text != "")
+            {
+                if (File.Exists(chip.Text))
+                {
+                    chipPfad = System.IO.Path.GetDirectoryName(chip.Text);
+                    chipSuchPfad = System.IO.Path.GetFullPath(chipPfad);
+                }
+                else
+                {
+                    MessageBox.Show("Datei existiert nicht");
+                    return;
+                }
+                fileBytes = File.ReadAllBytes(chip.Text);
+                filezeiger = 0;
+                magicString = new char[5];
+                int i;
+                for (i = 0; i < 5; i++)
+                {
+                    magicString[i] = (char)fileBytes[i];
+                }
+                filezeiger = filezeiger + i;
+                string tmp1 = new string(magicString);
+                string tmp2 = "<?xml";
+                if (!(tmp1 == tmp2))
+                {
+                    MessageBox.Show("Kein xml!");
+                    return;
+                }
+                else
+                {
+                    // patch original to work
+                    position = Locate(fileBytes, suchb);
+                    int p = position[0];
+                    for (i = 0; i < ersetzb.Length; i++)
+                    {
+                        fileBytes[i] = ersetzb[i];
+                    }
+                    for (i = ersetzb.Length; i < (p - 1); i++)
+                    {
+                        fileBytes[i] = (byte)' ';
+                    }
+                    // patch done
+                    string XMLfile = System.Text.Encoding.UTF8.GetString(fileBytes);
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(new StringReader(XMLfile));
+                    i = 0;
+                    var ADClist = new List<adcport>();
+                    XmlNode r1 = doc.SelectSingleNode("/Mcu");
+                    XmlNodeList listOfItems = doc.SelectNodes("/Mcu/Pin");
+                    foreach (XmlNode singleItem in listOfItems)
+                    {
+                        string port = singleItem.Attributes["Name"].InnerText;
+                        var Alist = new List<string>();
+                        foreach (XmlNode child1 in singleItem.ChildNodes)
+                        {
+                            if (child1.Name == "Signal")
+                            {
+                                string signal = child1.Attributes["Name"].InnerText;
+                                if (signal.Length > 3)
+                                {
+                                    string test = signal.Remove(3);
+                                    if (test == "ADC")
+                                    {
+                                        if(signal.IndexOf("EXTI")==-1)
+                                        Alist.Add(signal);
+                                    }
+                                }
+                            }
+                        }
+                        adcliste = Alist.ToArray();
+                        if(Alist.Count>0)
+                        {
+                            adcport adcport;
+                            if (port.IndexOf("_")>0)
+                            {
+                                port = port.Remove(port.IndexOf("_"));
+                            }
+                            if (port.IndexOf("-") > 0)
+                            {
+                                port = port.Remove(port.IndexOf("-"));
+                            }
+                            adcport.Port = port;
+                            string adsc="";
+                            for (i = 0; i < Alist.Count(); i++)
+                            {
+                                adsc = adsc + adcliste[i];
+                                if (i < (Alist.Count - 1))
+                                    adsc = adsc + "/";
+                            }
+                            adcport.ADCs = adsc;
+                            ADClist.Add(adcport);
+                        }
+                    }
+                    adcports = ADClist.ToArray();
+                    gpioladen.Enabled = true;
+                }
+            }
+        }
+    }
+}
